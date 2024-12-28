72c53e3b876a09943c38606b307e6ca1a4aab96e
barto9496 <amoghkatti9496@gmail.com>
2024-12-28 12:41:45 -0600

    fix: updated static files access

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/your-repository-name/" : "",
+  basePath: isProd ? "/your-repository-name" : "",
   output: "export",
-  distDir: "build",
-  /* config options here */
 };
 
 export default nextConfig;
