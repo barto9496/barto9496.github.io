e012abde24f7b000659f8ce78eb17bc3b73add67
barto9496 <amoghkatti9496@gmail.com>
2025-01-01 22:27:51 -0600

    feat(PORTFOLIO): updating the typewriteprops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,13 @@
 // import nextConfig from "../../next.config";
 //
 import Typewriter from "./components/TypeWriter/TypeWriter";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          <Typewriter text={"$Amogh_Umesh_Katti"} delay={100} />
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>© 2024 Amogh Umesh Katti</p>
-      </footer>
+    <div className="flex-col justify-center justify-items-center">
+      <Typewriter text={"$Amogh_Umesh_Katti"} delay={100} />
+      <Button variant="text">Outlined</Button>
     </div>
   );
 }
